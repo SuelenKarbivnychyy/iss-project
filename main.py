1804ccbb8b7c83f6de1b0d8b0994865e7586c33b
Suelen Matos <suelenmatosr@gmail.com>
2023-08-17 14:12:59 -0400

    Developed an project to send an email when the international space station is by my location and I can see it by looking to the sky

--- main.py
+++ main.py
@@ -1,43 +1,66 @@
 import requests
 from datetime import datetime
+import smtplib
+import config
+import time
 
-MY_LAT = 27.664827
-MY_LONG = -81.515755
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+MY_LAT = 28.058570
+MY_LONG = -82.689470
 
 
-print(iss_latitude, iss_longitude)
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-if 23 <= iss_latitude and 32 >= iss_latitude:
-    print("I can see it")
+    iss_latitude = float(data["iss_position"]["latitude"]) - 4
+    iss_longitude = float(data["iss_position"]["longitude"]) - 4
 
-#Your position is within +5 or -5 degrees of the ISS position.
-
-
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+    else:
+        return False
 
 
+def send_email():
+    """A function to send an email"""
 
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=config.my_email, password=config.password)
+        connection.sendmail(
+            from_addr=config.my_email,
+            to_addrs=config.send_to,
+            msg="Subject: Look To the sky\n\nHey, Its time to look up."
+        )
+
+    print("email sent")
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset_utc = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    if sunset_utc == 0:
+        sunset_florida = 20
+    else:
+        sunset_florida = sunset_utc - 4
+
+    hour = datetime.now().hour
+    if sunset_florida < hour < sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        send_email()
